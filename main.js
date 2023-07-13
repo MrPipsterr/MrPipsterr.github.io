8529e053ba1d2d7ffb4cd5f2f134ea9ef1baf1fc
MrPipsterr <farrelpalinggi@gmail.com>
2023-07-14 02:06:37 +0800

    fix: footer and bio update

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+let age = 18;
+
+const date = new Date();
+const currentMonth = date.getMonth() + 1;
+const targetDate = new Date(date.getFullYear(), 5, 1);
+
+// check if the current date has already passed June 1st or not
+if (currentMonth >= 6 && date >= targetDate) {
+  age += 1;
+}
+
+const paragraph = document.querySelector(".biodata p");
+paragraph.innerHTML = paragraph.innerHTML.replace("age", age);
